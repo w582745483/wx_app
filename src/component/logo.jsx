acbefa087e5f87383ff5ddf72759030c5c503d5a
w582745483 <41361509+w582745483@users.noreply.github.com>
2019-04-07 18:59:01 +0800

    up

--- src/component/logo.jsx
+++ src/component/logo.jsx
@@ -30,19 +30,7 @@ export default class Logo extends Component {
         this.props.history.push('/video')
     }
     handleClick4 = () => {
-        var xhr = new XMLHttpRequest()
-        let postData = {
-            time_line_content: 'ceshi1',
-            video_address: 'http://jsmov2.a.yximgs.com/upic/2019/02/22/20/BMjAxOTAyMjIyMDEzNTRfMjUzNzYzNjM1XzEwOTU1MDk1MzE1XzFfMw==_b_B9d6964c263cd934fd550c340e20d64ec.mp4?tag=1-1553492747-sp-0-ihsdp6lffb-13faf7ace9e0a5a9&type=hot',
-            video_pic_address: 'http://js2.a.yximgs.com/upic/2019/02/22/20/BMjAxOTAyMjIyMDEzNTRfMjUzNzYzNjM1XzEwOTU1MDk1MzE1XzFfMw==_Bf76625cd00d95bd7f77bbcc4461b7865.jpg?tag=1-1553492747-sp-0-vqrv2btwym-2dec32ecaadb7859&type=hot'
-        }
-        xhr.open('POST', "http://47.93.189.47:8818/WebService1.asmx/SendTimeLineBigVideo", true)
-        xhr.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                console.log(this.responseText)
-            }
-        }
-        xhr.send(JSON.stringify(postData));
+        this.props.history.push('/bigvideo')
     }
     handleRefresh = () => {
         let current = 0;
