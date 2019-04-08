4e902bcc1ae58c2a045214f0d97856ad8c922584
w582745483 <41361509+w582745483@users.noreply.github.com>
2019-04-08 22:36:18 +0800

    Update menu.jsx

--- src/component/menu.jsx
+++ src/component/menu.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import { Button,Avatar,List} from 'antd'
+import { Card, Avatar, List } from 'antd'
 
 import '../assets/css/main.css'
 import logo from '../assets/img/ng-scope.jpg'
 
 
-const Item=List.Item
+const Item = List.Item
+const data = [
+    {
+        title: 'Title 1',
+    },
+    {
+        title: 'Title 2',
+    },
+    {
+        title: 'Title 3',
+    },
+    {
+        title: 'Title 4',
+    },
+];
 export default class Menu extends Component {
     state = {
         wxid: '',
@@ -35,20 +49,24 @@ export default class Menu extends Component {
             <div>
                 <img src={logo} alt='logo' className='logo-img' />
                 <div className='login_box'>
-                    <div style={{ background: '#666',width:'100%',height:'25%'}}>
-                    <Avatar src={this.state.header} style={{ backgroundColor: '#87d068',marginTop:'25px', marginLeft:'10px'}} size="large" icon="user" /> 
+                    <div style={{ background: '#666', width: '100%', height: '25%' }}>
+                        <Avatar src={this.state.header} style={{ backgroundColor: '#87d068', marginTop: '25px', marginLeft: '10px' }} size="large" icon="user" />
                     </div>
                     <div>
-                        <List >
-                            <Item>
-                                <span>1232132</span>
-                            </Item>
-                            <Item>
-                                <span>1232132</span>
-                            </Item>  <Item>
-                                <span>1232132</span>
-                            </Item>
-                        </List>
+                        <List
+                            grid={{ gutter: 16, column: 2 }}
+                            dataSource={data}
+                            renderItem={item => (
+                                <List.Item>
+                                    <List.Item.Meta
+                                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                                        title={<a href="https://ant.design"></a>}
+                                      
+                                    />
+                                  
+                                </List.Item>
+                            )}
+                        />
                     </div>
                 </div>
 
