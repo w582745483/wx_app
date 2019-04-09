764692957e0de4458260117e873c532dbdf6ad6e
w582745483 <41361509+w582745483@users.noreply.github.com>
2019-04-09 18:02:13 +0800

    up

--- src/component/menu.jsx
+++ src/component/menu.jsx
@@ -3,27 +3,39 @@ import { Card, Avatar, List } from 'antd'
 
 import '../assets/css/main.css'
 import logo from '../assets/img/ng-scope.jpg'
-
-
+import ninevideo from '../assets/img/ninevideo.png'
+import bigvideo from '../assets/img/bigvideo.png'
+import customer from '../assets/img/customer.png'
+import develop from '../assets/img/develop.png'
 const Item = List.Item
 const data = [
     {
-        title: 'Title 1',
+        title: '九宫格',
+        img:ninevideo,
+        url: '/ninevideo'
     },
     {
-        title: 'Title 2',
+        title: '长视频',
+        img:bigvideo,
+        url: '/bigvideo'
     },
     {
-        title: 'Title 3',
+        title: '开发中',
+        img:customer
     },
     {
-        title: 'Title 4',
+        title: '客服',
+        img:develop
     },
 ];
 export default class Menu extends Component {
     state = {
         wxid: '',
         header: ''
+    }
+    handleClick=(url)=>{
+        this.props.history.push(url)
+        console.log(url)
     }
     componentWillMount() {
         fetch("http://47.93.189.47:8818/WebService1.asmx/GetUserWxidAndHeadImageUrl", {
@@ -52,18 +64,19 @@ export default class Menu extends Component {
                     <div style={{ background: '#666', width: '100%', height: '25%' }}>
                         <Avatar src={this.state.header} style={{ backgroundColor: '#87d068', marginTop: '25px', marginLeft: '10px' }} size="large" icon="user" />
                     </div>
-                    <div>
+                    <div  style={{background:'#ddd',height:'65%'}}>
                         <List
-                            grid={{ gutter: 16, column: 2 }}
+                            split={false}
+                            grid={{ gutter: 16, column: 3 }}
                             dataSource={data}
                             renderItem={item => (
                                 <List.Item>
-                                    <List.Item.Meta
-                                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                                        title={<a href="https://ant.design"></a>}
-                                      
-                                    />
-                                  
+                                    <div >
+                                        <img onClick={()=>this.handleClick(item.url)} style={{width:'50px',marginLeft:'30px',marginTop:'20px'}} src={item.img}></img>
+                                        <div style={{textAlign:'center'}}>
+                                            <span style={{lineHeight:'1.15',fontSize:'1rem',marginLeft:'-10px'}}>{item.title}</span>
+                                        </div>
+                                    </div>
                                 </List.Item>
                             )}
                         />
